c0fb315e2ffbf51350867d6716b6745b35ac234e
sasin janchom <sjanchom@sasins-MacBook-Pro.local>
2017-03-12 23:51:45 +0700

    initial project

---
+++ Project_Store/Controllers/StoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_Store.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Store()
+        {
+            return View();
+        }
+    }
+}
